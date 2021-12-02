21b75442870a03053e0b40f55a426a569df1e38a
mustyfe <mustyfe@gmail.com>
2021-12-03 00:58:43 +0300

    manav kasa programı eklendi

---
+++ src/Giris/ManavKasaProgami.java
@@ -0,0 +1,28 @@
+package Giris;
+import java.util.Scanner;
+public class ManavKasaProgami {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        double armut, elma, domates, muz, patlıcan;
+
+        System.out.println("LÜTFEN ALINAN ÜRÜNLERİ KG CİNSİNDEN GİRİNİZ");
+        System.out.print("Armut Kaç Kilo? : ");
+        armut = inp.nextDouble();
+
+        System.out.print("Elma Kaç Kilo? : ");
+        elma = inp.nextDouble();
+
+        System.out.print("Domates Kaç Kilo? : ");
+        domates = inp.nextDouble();
+
+        System.out.print("Muz Kaç Kilo? : ");
+        muz = inp.nextDouble();
+
+        System.out.print("Patlıcan Kaç Kilo? : ");
+        patlıcan = inp.nextDouble();
+
+        double tutar = ((armut * 2.14) + (elma * 3.67) + (domates * 1.11) + (muz * 0.95) + (patlıcan * 5));
+
+        System.out.print("Toplam Tutar: " + tutar + "TL");
+    }
+}
